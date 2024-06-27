642c0834245259b9df87f2740b0aec014df423bf
swathi <159229827+panchiredddy@users.noreply.github.com>
2024-06-27 09:42:16 +0530

    increment operator

---
+++ increment.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+	/*int i=5;
+	i++;
+	printf("%d\n",i);
+	int j=5;
+    ++j;
+    printf("%d\n",j);
+    int k=5;
+    printf("%d\n",k++);
+    int m=5;
+    printf("%d\n",++m);
+    int n=5;
+    printf("%d%d%d\n",n++,n++,n++);
+    int l=5;
+    printf("%d%d%d\n",++l,++l,++l);
+    int a=5;
+    printf("%d%d%d\n",++a,a++,++a);
+    int a=5;
+    printf("%d%d%d%d%d\n",++a,a++,a++,++a,a++);
+    int a=6,b=4,c;
+    c=a++;
+    b=++c;
+    a=b++;
+    a++;
+    printf("%d\n%d\n%d\n",a,b,c);*/
+    int i=5;
+	i--;
+	printf("%d\n",i);
+	int j=5;
+    --j;
+    printf("%d\n",j);
+    int k=5;
+    printf("%d\n",k--);
+    int m=5;
+    printf("%d\n",--m);
+	return 0;
+}
