b2a562ef54a142b35a2faad1c411aacb7e43c183
swathi <159229827+panchiredddy@users.noreply.github.com>
2024-07-03 08:13:06 +0530

    reverse num

---
+++ reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n,digit,rev=0;
+	printf("enter:");
+	scanf("%d",&n);
+	while(n)
+	{
+	  	digit=n%10;
+	  	rev=rev*10+digit;
+	  	n=n/10;
+	}
+	printf("%d",rev);
+	return 0;
+}
